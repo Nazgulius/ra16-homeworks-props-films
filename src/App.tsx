8a782e11b167b7973ea743f71ed15a36628eb8e9
Maksim Chalyh <nazgulius@yandex.ru>
2025-03-24 23:56:26 +0300

    task OK

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,92 @@
-// import { useState } from 'react'
 import './App.css'
 import Stars from './components/Stars'
+import imgSuperman from './components/img/superman.jpg'
 
 function App() {
-  let count: number = 1;
+  let count1: number = 0;
+  let count2: number = 2;
+  let count3: number = 5;
+  let count4: number = 6;
   
   return (
     <>
       <div className="block">
-        <img className="block-poster" src="#" alt="poster" />
+        <img className="block-poster" src={imgSuperman} alt="poster" />
         <div className='blosk-container-main'>
           <h2 className='blosk-title'>Супермен</h2>
-          <Stars count={count} />
+          <Stars count={count1} />
+          <button className='blosk-btn-bye'>Купить 1299Р</button>
+          <button className='blosk-btn-more'>Подробнее -</button>
+        </div>
+        <div className='blosk-container-svg'>
+          {/* сердце пустое */}
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="svg heart1" viewBox="0 0 16 16">
+            <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
+          </svg>
+          {/* сердце целое */}
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 512 512" className="svg heart2" fill="currentColor">
+            <path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z" />
+          </svg>
+          {/* поделиться */}
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="currentColor" className="svg share" >
+            <path d="M11 6C12.6569 6 14 4.65685 14 3C14 1.34315 12.6569 0 11 0C9.34315 0 8 1.34315 8 3C8 3.22371 8.02449 3.44169 8.07092 3.65143L4.86861 5.65287C4.35599 5.24423 3.70652 5 3 5C1.34315 5 0 6.34315 0 8C0 9.65685 1.34315 11 3 11C3.70652 11 4.35599 10.7558 4.86861 10.3471L8.07092 12.3486C8.02449 12.5583 8 12.7763 8 13C8 14.6569 9.34315 16 11 16C12.6569 16 14 14.6569 14 13C14 11.3431 12.6569 10 11 10C10.2935 10 9.644 10.2442 9.13139 10.6529L5.92908 8.65143C5.97551 8.44169 6 8.22371 6 8C6 7.77629 5.97551 7.55831 5.92908 7.34857L9.13139 5.34713C9.644 5.75577 10.2935 6 11 6Z" fill="#000000" />
+          </svg>
+        </div>
+      </div>
+
+      <div className="block">
+        <img className="block-poster" src={imgSuperman} alt="poster" />
+        <div className='blosk-container-main'>
+          <h2 className='blosk-title'>Супермен</h2>
+          <Stars count={count2} />
+          <button className='blosk-btn-bye'>Купить 1299Р</button>
+          <button className='blosk-btn-more'>Подробнее -</button>
+        </div>
+        <div className='blosk-container-svg'>
+          {/* сердце пустое */}
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="svg heart1" viewBox="0 0 16 16">
+            <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
+          </svg>
+          {/* сердце целое */}
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 512 512" className="svg heart2" fill="currentColor">
+            <path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z" />
+          </svg>
+          {/* поделиться */}
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="currentColor" className="svg share" >
+            <path d="M11 6C12.6569 6 14 4.65685 14 3C14 1.34315 12.6569 0 11 0C9.34315 0 8 1.34315 8 3C8 3.22371 8.02449 3.44169 8.07092 3.65143L4.86861 5.65287C4.35599 5.24423 3.70652 5 3 5C1.34315 5 0 6.34315 0 8C0 9.65685 1.34315 11 3 11C3.70652 11 4.35599 10.7558 4.86861 10.3471L8.07092 12.3486C8.02449 12.5583 8 12.7763 8 13C8 14.6569 9.34315 16 11 16C12.6569 16 14 14.6569 14 13C14 11.3431 12.6569 10 11 10C10.2935 10 9.644 10.2442 9.13139 10.6529L5.92908 8.65143C5.97551 8.44169 6 8.22371 6 8C6 7.77629 5.97551 7.55831 5.92908 7.34857L9.13139 5.34713C9.644 5.75577 10.2935 6 11 6Z" fill="#000000" />
+          </svg>
+        </div>
+      </div>
+
+      <div className="block">
+        <img className="block-poster" src={imgSuperman} alt="poster" />
+        <div className='blosk-container-main'>
+          <h2 className='blosk-title'>Супермен</h2>
+          <Stars count={count3} />
+          <button className='blosk-btn-bye'>Купить 1299Р</button>
+          <button className='blosk-btn-more'>Подробнее -</button>
+        </div>
+        <div className='blosk-container-svg'>
+          {/* сердце пустое */}
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="svg heart1" viewBox="0 0 16 16">
+            <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
+          </svg>
+          {/* сердце целое */}
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 512 512" className="svg heart2" fill="currentColor">
+            <path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z" />
+          </svg>
+          {/* поделиться */}
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="currentColor" className="svg share" >
+            <path d="M11 6C12.6569 6 14 4.65685 14 3C14 1.34315 12.6569 0 11 0C9.34315 0 8 1.34315 8 3C8 3.22371 8.02449 3.44169 8.07092 3.65143L4.86861 5.65287C4.35599 5.24423 3.70652 5 3 5C1.34315 5 0 6.34315 0 8C0 9.65685 1.34315 11 3 11C3.70652 11 4.35599 10.7558 4.86861 10.3471L8.07092 12.3486C8.02449 12.5583 8 12.7763 8 13C8 14.6569 9.34315 16 11 16C12.6569 16 14 14.6569 14 13C14 11.3431 12.6569 10 11 10C10.2935 10 9.644 10.2442 9.13139 10.6529L5.92908 8.65143C5.97551 8.44169 6 8.22371 6 8C6 7.77629 5.97551 7.55831 5.92908 7.34857L9.13139 5.34713C9.644 5.75577 10.2935 6 11 6Z" fill="#000000" />
+          </svg>
+        </div>
+      </div>
+
+      <div className="block">
+        <img className="block-poster" src={imgSuperman} alt="poster" />
+        <div className='blosk-container-main'>
+          <h2 className='blosk-title'>Супермен</h2>
+          <Stars count={count4} />
           <button className='blosk-btn-bye'>Купить 1299Р</button>
           <button className='blosk-btn-more'>Подробнее -</button>
         </div>
--- src/components/Stars.tsx
+++ src/components/Stars.tsx
@@ -4,19 +4,21 @@ interface StarProps {
 
 export default function Star({ count }: StarProps) {
 
-  const star: HTMLElement = (
-    <li>
-      <svg fill="#D3BCA2" height="28" viewBox="0 0 18 18" width="28" xmlns="http://www.w3.org/2000/svg">
-        <path d="M9 11.3l3.71 2.7-1.42-4.36L15 7h-4.55L9 2.5 7.55 7H3l3.71 2.64L5.29 14z" />
-        <path d="M0 0h18v18H0z" fill="none" />
-      </svg>
-    </li>
+  if (count < 1 || count > 5) {
+    console.log("Рейтинг может быть от 1 до 5 звёзд.");
+    return;
+  }
+
+  const star: React.ReactElement = (
+    <svg fill="#D3BCA2" height="28" viewBox="0 0 18 18" width="28" xmlns="http://www.w3.org/2000/svg">
+      <path d="M9 11.3l3.71 2.7-1.42-4.36L15 7h-4.55L9 2.5 7.55 7H3l3.71 2.64L5.29 14z" />
+      <path d="M0 0h18v18H0z" fill="none" />
+    </svg>
   );
   
-  const arrayStars = Array.from({ length: count }, (_, index) => (  
+  const arrayStars: React.ReactElement[] = Array.from({ length: count }, (_, index) => (  
     <li key={index}>{star}</li>  
   ));
-
 
   return (
     <>
